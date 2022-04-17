ecbf6f841c297dc7006e4a2135c437a86fb5796c
Hikaru Y <47855854+hikaru-y@users.noreply.github.com>
2022-04-17 18:05:57 +0900

    Initialize svelteshim

---
+++ bin/zip.sh
@@ -0,0 +1,18 @@
+declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
+mkdir -p "$DIR/build"
+
+if [[ "$1" =~ ^-?a$ ]]; then
+  # for uploading to AnkiWeb
+  declare addon_id=''
+else
+  # for installing myself
+  declare addon_id='new_format_pack'
+fi
+
+"$DIR/bin/compile.sh"
+
+cd "$DIR/dist"
+
+rm -rf ./**"/__pycache__"
+
+zip -r "$DIR/build/$addon_id.ankiaddon" *
---
+++ web/editor.js
@@ -0,0 +1,14 @@
+import StrikeThrough from "./StrikeThrough.svelte";
+import Paragraph from "./Paragraph.svelte";
+import HorizontalRuler from "./HorizontalRuler.svelte";
+import CodeBlock from "./CodeBlock.svelte";
+
+$editorToolbar.then((editorToolbar) => {
+  editorToolbar.formatInlineButtons.appendButton(
+    { component: StrikeThrough },
+    2
+  );
+  editorToolbar.formatBlockButtons.appendButton({ component: Paragraph });
+  editorToolbar.formatBlockButtons.appendButton({ component: HorizontalRuler });
+  editorToolbar.formatBlockButtons.appendButton({ component: CodeBlock });
+});
